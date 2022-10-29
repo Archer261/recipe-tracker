1e29b48a14989c1ce16f13a458b6f1fe052843db
Joseph Archer <viewtiful.kja@gmail.com>
2022-10-29 11:05:58 -0400

    need to pull down changes

---
+++ client/src/components/SampleCard.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+
+function SampleCard({ rec }) {
+
+    return (
+        <div className='flex flex-col'>
+            <div class="grid justify-items-stretch ...">
+                <h2 className='justify-self-center'>Sample Recipe Card</h2>
+            </div>
+            <div className="card card-compact w-96 bg-base-100 drop-shadow-2xl m-3">
+
+                <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
+                <div className="card-body">
+                    <h2 className="card-title"></h2>
+                    <p></p>
+
+                    <div className="card-actions justify-between">
+                        <button className="btn btn-primary">Open</button>
+                        <button className="btn btn-circle">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    );
+}
+
+export default SampleCard;
--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -7,7 +7,7 @@ function Profile() {
     return (
         <div class="bg-secondary">
             <div class="grid justify-items-stretch ...">
-                <h1 className="justify-self-center text-2xl text-white">My Recipies</h1>
+                <h1 className="justify-self-center text-3xl md:text-4xl font-medium mb-2 text-white">My Recipies</h1>
             </div>
             <RecipeList />
 
