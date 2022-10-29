13ca4c73ea8c438dd8011727987039e159877a0b
Joseph Archer <viewtiful.kja@gmail.com>
2022-10-29 14:07:58 -0400

    remade branch

---
+++ client/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './components/App'
+import { BrowserRouter } from 'react-router-dom'
+import Profile from './pages/Profile';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <App />
+
+);
