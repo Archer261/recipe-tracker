f1d3e862c1cf41829f40d33ecc5075537fb214ac
Joseph Archer <viewtiful.kja@gmail.com>
2022-10-29 09:57:09 -0400

    new recipe page functionality

--- src/pages/NewRecipe.js
+++ src/pages/NewRecipe.js
@@ -1,10 +1,22 @@
-import React from 'react'
+import { React, useState } from 'react'
 import { Outlet, Link } from 'react-router-dom'
+
+
+
+
+
 
 
 function NewRecipe() {
 
     let recObj = {}
+
+    const [counter, setCounter] = useState(0);
+
+    const handleClick = () => {
+        setCounter(counter + 1);
+        console.log(counter);
+    };
 
     return (
         <>
@@ -14,12 +26,15 @@ function NewRecipe() {
             <div className='flex flex-col mx-5 my-5'>
 
                 <div class="form-control">
-                    <span class="label-text">Recipe Name</span>
-                    <input type="text" placeholder="Type here"
-                        class="input input-md input-bordered input-info w-full max-w-sm m-2 bg-white" />
+                    <label className="input-group input-group-sm my-2">
+                        <span class="label-text">Recipe Name</span>
+                        <input type="text" placeholder="Type here" className="input input-bordered input-sm w-full max-w-xs my-2 bg-white" />
+                    </label>
 
-                    <input type="text" placeholder="Type here"
-                        class="input input-md input-bordered input-info w-full max-w-sm m-2 bg-white" />
+                    <label className="input-group input-group-sm my-2">
+                        <span>Description</span>
+                        <input type="text" placeholder="Type here" className="input input-bordered input-sm w-full max-w-xs my-2 bg-white" />
+                    </label>
                 </div>
 
                 <div className="divider"></div>
@@ -27,11 +42,14 @@ function NewRecipe() {
 
                 <h2 className='justify-self-center'>Steps</h2>
                 <div class="form-control">
-                    <input type="text" placeholder="Type here"
-                        class="input input-md input-bordered input-info w-full max-w-sm m-2 bg-white" />
+                    <input type="text" placeholder="Type here" className="input input-bordered input-sm w-full max-w-xs m-2 bg-white" />
 
-                    <input type="text" placeholder="Type here"
-                        class="input input-md input-bordered input-info w-full max-w-sm m-2 bg-white" />
+                    <button onClick={handleClick}><svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6"></path></svg></button>
+                    {Array.from(Array(counter)).map((c, index) => {
+                        return <div className='flex'><input type="text" placeholder="Type here" className="input input-bordered input-sm w-full max-w-xs m-2 bg-white" /> <button className="btn btn-circle">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
+                        </button></div>;
+                    })}
                 </div>
 
                 <div class="flex justify-between ...">
