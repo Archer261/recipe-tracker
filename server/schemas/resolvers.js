75dd7abcfd7e126ca39ea52829870215f6463527
Dustin Tierney <dustintierney@rockethomes.com>
2022-11-01 20:32:03 -0400

    updated app with apollo config

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -75,8 +75,7 @@ const resolvers = {
             return recipe;
         },
         recipes: async () => {
-            const recipes = recipe
-                .find()
+            const recipes = Recipe.find()
                 .sort({ createdAt: -1 })
                 .populate('ingredients')
                 .populate('steps')
